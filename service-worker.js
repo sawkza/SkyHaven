abc8481278769e5a87e869c9c73776a088aec059
Saw K Za <sawkza25505@gmail.com>
2024-11-14 12:17:30 +0800

    Updated HTML for notification testing and additional text

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,40 @@
 self.addEventListener('install', event => {
   console.log('Service Worker installing...');
-  // Cache static assets here if necessary
+  event.waitUntil(
+    caches.open('skyhaven-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/static/icon-192x192.png',
+        '/static/icon-512x512.png',
+        '/manifest.json'
+      ]);
+    })
+  );
 });
 
 self.addEventListener('activate', event => {
   console.log('Service Worker activating...');
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== 'skyhaven-cache') {
+            console.log('Clearing old cache:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
 });
 
 self.addEventListener('fetch', event => {
-  // Intercept network requests (optional)
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    }).catch(() => {
+      return caches.match('/index.html');
+    })
+  );
 });
